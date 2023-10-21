a3b847872245ac55f675869140d2d5c63eeb75de
laverienko <carlos.gutierrez.gi@hotmail.com>
2023-10-21 13:13:45 -0500

    se agrego la hoja de estilo y el archivo .js

---
+++ MiPlata/JS/consultar_saldo.js
@@ -0,0 +1,11 @@
+
+
+let saldos = [];
+let consultas = 0
+
+if(resultado != resultado){
+    consultas =+ 1;
+    for(let i = 0; i<consultas;i++){
+        resultado /* Acá voy, no se lo he terminado de pensar */
+    }
+}
