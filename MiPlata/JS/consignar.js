d1d1b75ef5c1cc5a2bf13a5d04fe8a03f1096dbc
Caroluquec <caroluque13@gmail.com>
2023-10-24 20:59:35 -0500

    Se hace Js de cambio de contraseña y consignación

---
+++ MiPlata/JS/consignar.js
@@ -0,0 +1,9 @@
+
+function consignar(){
+    let saldo_ant=0
+    let nuevo_saldo=0
+    let valor=parseInt(document.getElementById('valor').value)
+    nuevo_saldo=saldo_ant + valor
+    
+    alert(`Transacción exitosa. Este es tu nuevo saldo ${nuevo_saldo}, este es tu saldo anterior ${saldo_ant}. \n Valor de consignación ${valor}`)
+}
