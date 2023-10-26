0575e5c9bdcc2636de90d370660f31f73fef8fea
Miguel Piedrahita <tfkmap543@gmail.com>
2023-10-26 01:42:32 -0500

    se modifica la estructura del vector usuarios

--- MiPlata/JS/js.js
+++ MiPlata/JS/js.js
@@ -1,8 +1,23 @@
-const usuarios=[123,22,123];
+let usuarios=[
+    {   
+        id : 1234,
+        username : 'miguel piedrahita',
+        email : 'map543@hotmail.com',
+        password : '4321',
+        saldo : 300000
+    },
+    {
+        id : 4321,
+        username  : 'diego escobar',
+        email : 'juand@gmail.com',
+        password : '1234',
+        saldo : 10000000
+    }
 
+];
 function ingresar(){
-    let user=usuarios[1]
-    let pwd=usuarios[2]
+    let user=usuarios[0].username
+    let pwd=usuarios[0].password
 
     if(document.getElementById('inpt-usuario').value==user && document.getElementById('inpt-contraseña').value==pwd){
         window.location="menuprincipal.html";
