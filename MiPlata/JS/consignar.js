e6ddde3276bcadd2f163e8ae0e1faefb09cdfe5f
Caroluquec <caroluque13@gmail.com>
2023-10-26 20:12:56 -0500

    Se modifica el modal de consiganrl nuevamente por el mensaje de alert

--- MiPlata/JS/consignar.js
+++ MiPlata/JS/consignar.js
@@ -1,23 +1,8 @@
-
 function consignar(){
     let saldo_ant=0
     let nuevo_saldo=0
     let valor=parseInt(document.getElementById('valor').value)
     nuevo_saldo=saldo_ant + valor
     
-    
+    alert(`Transacción exitosa. Este es tu nuevo saldo ${nuevo_saldo}, este es tu saldo anterior ${saldo_ant}. \n Valor de consignación ${valor}`)
 }
-
-let modal=document.getElementById("modal")
-let otr_final=document.getElementById("trfinal")
-let confirmar=document.getElementById("btn-confi")
-
-
-confirmar.addEventListener("click", (e)=>{
-    e.preventDefault
-    otr_final.classList.remove('modal-disabledtr')
-
-})
-
-
-
