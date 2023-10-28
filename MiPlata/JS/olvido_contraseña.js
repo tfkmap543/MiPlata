42753f01ea8bed2dec791d02d19e404c2f662efb
DiegoEsc2322 <123429180+DiegoEsc2322@users.noreply.github.com>
2023-10-28 01:49:46 -0500

    enlaces, olvido contraseña y solucion enlace de las pags de caro

---
+++ MiPlata/JS/olvido_contraseña.js
@@ -0,0 +1,13 @@
+function cambiar_clave(){
+
+    /*   let actual=1234
+      let actual_1=parseInt(document.getElementById('actual').value)
+      let nueva=parseInt(document.getElementById('nueva').value)
+      let confirme=parseInt(document.getElementById('confirme').value) */
+  
+   
+     
+      alert("Se te envio la confimación de cambio de clave, si no la confirmas en 15 minutos, esta acción se deshara")// Aquí se inserta el modal cuando este listo
+     
+  
+  }
